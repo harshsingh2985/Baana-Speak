8023d1b1d0820af59f63ad53bf12edb5ce0981f5
Harsh Singh <88739283+uniqueharhsingh@users.noreply.github.com>
2022-05-02 21:39:12 -0700

    input and processing

--- App.js
+++ App.js
@@ -1,16 +1,15 @@
-let btnTranslate=document.querySelector('#btntranslate');
+var translateButton = document.querySelector("#translate-button");
 
 var txtInput=document.querySelector("#txt-input");
-
 
 console.log(txtInput);
 
 
+function buttonClickHandler() {
+    console.log("button clicked");
+    console.log("input",txtInput.value)
 
-// // Call-back function
-// function clickHandler(){
-//     console.log("you have been clicked ")
+}
+translateButton.addEventListener("click", buttonClickHandler);
 
-// }
 
-// btnTranslate.addEventListener("click",clickHandler);
